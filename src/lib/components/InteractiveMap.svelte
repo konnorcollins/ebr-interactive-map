d5d49c2003291fea7cfb525d31e7979172be6c8d
Konnor Collins <konnor.s.collins@gmail.com>
2026-01-18 12:58:11 -0700

    Initial conversion to svelte

---
+++ src/lib/components/InteractiveMap.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+    interface Props {
+        x: number,
+        y: number,
+        mapImage: string,
+    }
+
+    let { x, y, mapImage }: Props = $props();
+
+
+    let svgElem = $state<SVGElement>();
+    let svgWidth = $derived(svgElem?.getBoundingClientRect().width);
+    let svgHeight = $derived(svgElem?.getBoundingClientRect().height);
+
+    let map = $state<SVGImageElement>();
+    let mapScaleFactor = $derived(map?.transform.baseVal);
+
+
+    let showLocationCardNumbers = $state(false);
+
+</script>
+
+<div id="mapDiv">
+    <svg bind:this={svgElem} width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" id="mapSVG">
+        <!-- Define some global effects -->
+        <defs>
+            <filter id="dropShadow">
+                <feDropShadow dx="2" dy="2" stdDeviation="2" />
+            </filter>
+        </defs>
+
+        <image bind:this={map} id="mapImage" width="6000" height="7800" {x} {y} href={mapImage} transform="scale(0.4)" />
+    </svg>
+</div>
---
+++ src/routes/valley_map/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import ValleyMapImage from "$lib/assets/EBR001_Valley Map_eng.jpg"
+    import InteractiveMap from "$lib/components/InteractiveMap.svelte";
+
+</script>
+
+
+<InteractiveMap x={-1310} y={-1445} mapImage={ValleyMapImage} />
